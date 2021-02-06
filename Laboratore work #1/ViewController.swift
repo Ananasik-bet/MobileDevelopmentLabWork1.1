5fbf719ba9d86ca121f2655e17430404f51e262a
Vlad Diachenko <vlad.diachenko.01.2001@gmail.com>
2021-02-06 18:22:52 +0200

    Add files for Laboratory Work 1.1

---
+++ Laboratore work #1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Laboratore work #1
+//
+//  Created by Владислав Дяченко on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
